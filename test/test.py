df5612478065d263aecc1364a2c91396b33ccaf4
Kogia-sima <orcinus4627@gmail.com>
2018-06-03 23:51:43 +0900

    Add test script

---
+++ test/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import StringFilter
+
+def test_rabbit_and_turtle() -> None:
+    sf = StringFilter.StringFilter('rabbit and turtle')
+    assert sf.is_valid('rabbit vs turtle')
+    assert not sf.is_valid('Sea turtles were released to the ocean.')
+
+
+def test_null_str() -> None:
+    sf = StringFilter.StringFilter('')
+    assert sf.is_valid('Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.')
+
+    sf = StringFilter.StringFilter('buffalo')
+    assert not sf.is_valid('')
